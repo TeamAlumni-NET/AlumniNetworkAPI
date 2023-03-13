6b9652dca6fdded7010443ec138ee37a552414e4
KipaTa <kirsi.tainio@gmail.com>
2023-03-13 13:52:45 +0200

    feat: EventDto, EventUserDto, GroupDto

---
+++ Models/DTOs/EventDtos/EventDto.cs
@@ -0,0 +1,22 @@
+﻿using AlumniNetworkAPI.Models.Models;
+
+namespace AlumniNetworkAPI.Models.DTOs.EventDtos
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool AllowGuests { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int EventCreatorId { get; set; }
+        public User? EventCreator { get; set; }
+        public List<int>? Topics { get; set; }
+        public List<int>? EventUsers { get; set; }
+        public List<int>? Posts { get; set; }
+        public List<int>? Rsvps { get; set; }
+        public List<int>? Groups { get; set; }
+    }
+}
