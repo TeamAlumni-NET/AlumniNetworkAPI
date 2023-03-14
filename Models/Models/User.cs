6904b41ce2d06550e214cb23eedb4c1c21757cab
Marco Angeli <81585043+DeferredMonk@users.noreply.github.com>
2023-03-14 13:41:09 +0200

    feat: added username to user model. Made status, bio, fun fact, and picurl nullable

--- Models/Models/User.cs
+++ Models/Models/User.cs
@@ -6,13 +6,14 @@ namespace AlumniNetworkAPI.Models.Models
     {
         public int Id { get; set; }
         [MaxLength(50)]
+        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Status { get; set; }
-        public string Bio { get; set; }
-        public string FunFact { get; set; }
+        public string? Status { get; set; }
+        public string? Bio { get; set; }
+        public string? FunFact { get; set; }
         [Url]
-        public string PictureUrl { get; set; }
+        public string? PictureUrl { get; set; }
         public IList<EventUser> EventUsers { get; set; }
         public ICollection<Event> Events { get; set; }
         public ICollection<Group> Groups { get; set; }
