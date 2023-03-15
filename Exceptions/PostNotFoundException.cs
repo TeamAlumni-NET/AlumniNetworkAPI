2a303da9355472494e9d86583e3851fb7bc2f863
KipaTa <kirsi.tainio@gmail.com>
2023-03-15 11:47:48 +0200

    Feat: Post api functions edits

---
+++ Exceptions/PostNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace AlumniNetworkAPI.Exceptions
+{
+    public class PostNotFoundException: Exception
+    {
+        public PostNotFoundException(int id) : base($"Post with id {id} was not found")
+        {
+
+        }
+    }
+}
