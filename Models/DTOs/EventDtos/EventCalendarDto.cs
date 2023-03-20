f263c71bb40494d47892d34755b50544b662bb6b
Marco Angeli <81585043+DeferredMonk@users.noreply.github.com>
2023-03-20 15:21:42 +0200

    fix: fixed event calendar dto

--- Models/DTOs/EventDtos/EventCalendarDto.cs
+++ Models/DTOs/EventDtos/EventCalendarDto.cs
@@ -4,7 +4,7 @@
     {
         public int id { get; set; }
         public string title { get; set; }
-        public string description { get; set; }
+        public string Description { get; set; }
         public DateTime start { get; set; }
         public DateTime? end { get; set; }
     }
