ce4342800ae4ab3fea86be0d6d6ce37327d41eb2
KipaTa <kirsi.tainio@gmail.com>
2023-03-22 13:34:36 +0200

    Fix: IGroup Servce fix

--- Services/Groups/IGroupService.cs
+++ Services/Groups/IGroupService.cs
@@ -5,6 +5,6 @@ namespace AlumniNetworkAPI.Services.Groups
 {
     public interface IGroupService : ICrudService<Group, int>
     { 
-        Task<Group> AddUserToGroup(int groupId, int userId);
+        Task<Group> AddUserToGroup(int id, int userId);
     }
 }
