fa9ab0ab93bd6ad2907ab4b163458bdfd6f2fe84
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-03-22 14:49:50 +0200

    simplepostdto fix

--- Models/DTOs/PostDtos/TimelinePostDto.cs
+++ Models/DTOs/PostDtos/TimelinePostDto.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public DateTime TimeStamp { get; set; }
         public string? Title { get; set; }
-        public string Content {  get; set; }
+        public string? Content {  get; set; }
         public string User { get; set; }
         public string? Topic { get; set; }
         public string? Group { get; set; }
