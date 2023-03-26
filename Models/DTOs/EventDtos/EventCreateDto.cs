e9d2537db2bada98762524f328a6d2b9825db284
KipaTa <kirsi.tainio@gmail.com>
2023-03-26 14:24:02 +0300

    Feat: Create event

---
+++ Models/DTOs/EventDtos/EventCreateDto.cs
@@ -0,0 +1,20 @@
+﻿namespace AlumniNetworkAPI.Models.DTOs.EventDtos
+{
+    public class EventCreateDto
+    {
+        public int Id { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool AllowGuests { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int EventCreatorId { get; set; }
+
+        //public List<int>? Topics { get; set; }
+        //public List<int>? EventUsers { get; set; }
+        //public List<int>? Posts { get; set; }
+        //public List<int>? Rsvps { get; set; }
+        //public List<int>? Groups { get; set; }
+    }
+}
